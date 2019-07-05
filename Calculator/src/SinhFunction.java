b7344fa1960289592ab7ddb4498b3e5a92446ce0
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-07-05 18:46:32 -0400

    init

---
+++ Calculator/src/SinhFunction.java
@@ -0,0 +1,12 @@
+/**
+ * This class  implements function sinh , please refer readme file for the details about the
+ * function
+ *
+ * @author :prashantkumar patel
+ */
+public class SinhFunction {
+
+  public static void main(String[] args) {
+
+  }
+}
