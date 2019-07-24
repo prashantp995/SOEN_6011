30a6121bdc761dcc198781586c7bc892f55674c4
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-07-24 18:55:41 -0400

    added new test case

--- Calculator/src/SinhFunctionTest.java
+++ Calculator/src/SinhFunctionTest.java
@@ -1,27 +1,48 @@
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SinhFunctionTest {
+
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
+
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
+
+  @After
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
 
   @Test
   public void InitCalculation_invalidInput() {
     SinhFunction sinhFunction = new SinhFunction();
     ByteArrayInputStream input = new ByteArrayInputStream("My string".getBytes());
     System.setIn(input);
-    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    final PrintStream originalOut = System.out;
-    final PrintStream originalErr = System.err;
-    System.setOut(new PrintStream(outContent));
-    System.setErr(new PrintStream(errContent));
     sinhFunction.InitCalculation();
-
     Assert.assertTrue(UIMessages.ERROR_MESSAGE.equalsIgnoreCase(outContent.toString().trim()));
-    System.setOut(originalOut);
-    System.setErr(originalErr);
+    System.setIn(System.in);
+  }
+
+  @Test
+  public void InitCalculation_validInput() {
+    SinhFunction sinhFunction = new SinhFunction();
+    ByteArrayInputStream input = new ByteArrayInputStream("1".getBytes());
+    System.setIn(input);
+    sinhFunction.InitCalculation();
+    System.out.println(outContent.toString().trim().contains("1.17520119364"));
+    System.setIn(System.in);
   }
 
 
