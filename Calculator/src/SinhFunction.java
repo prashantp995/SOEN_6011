727e5f30e906db9e2ff920583fa8f86895b29a78
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-07-24 16:54:06 -0400

    sinhx function implementation wip

--- Calculator/src/SinhFunction.java
+++ Calculator/src/SinhFunction.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This class  implements function sinh , please refer readme file for the details about the
  * function
@@ -6,7 +8,72 @@
  */
 public class SinhFunction {
 
-  public static void main(String[] args) {
+  private static final int maxSteps = 15;
+  private static final double divisor = 2d;
 
+  /**
+   * This function calculate e power x based on Taylor series, First 15 steps of Taylor series will
+   * be considered to calculate e power x with good accuracy
+   *
+   * @param x real number provided by user
+   * @return calculated value of e power x
+   */
+  private static double ePowerX(double x) {
+    double result = 1;
+
+    for (int i = SinhFunction.maxSteps - 1; i > 0; --i) {
+      result = 1 + x * result / i;
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    printWelcomeMessage();
+    Scanner scanner = new Scanner(System.in);
+    if (scanner.hasNextDouble()) {
+      double x = scanner.nextDouble();
+      if (Validator.validateInputRange(x)) {
+        double ePowerx = ePowerX(x);
+        double ePowerMinusx = ePowerX(-x);
+        validateAndCalculateSinhx(x, ePowerx, ePowerMinusx);
+      } else {
+        System.out.println(UIMessages.ERROR_MESSAGE_RANGE);
+      }
+
+    } else {
+      System.out.println(UIMessages.ERROR_MESSAGE);
+    }
+
+  }
+
+  private static void validateAndCalculateSinhx(double x, double ePowerx, double ePowerMinusx) {
+    if (Validator.validateOutputRange(ePowerx) && Validator.validateOutputRange(ePowerMinusx)) {
+      double result = calculateSinh(ePowerx, ePowerMinusx);
+      System.out.print(UIMessages.SUCCESS_MESSAGE + x + " is ");
+      System.out.printf("%.15f", result);
+    }
+  }
+
+
+  /**
+   * This function calculates sinh(x) based on formula , sinh(x) = (e^x - e^-x)/2
+   *
+   * @return result of sinh based on input
+   */
+  private static double calculateSinh(double ePowerx, double ePowerMinusx) {
+    if (Validator.validateOutputRange(ePowerx)) {
+      Validator.validateOutputRange(ePowerMinusx);
+    }
+
+    return (ePowerx - ePowerMinusx) / divisor;
+  }
+
+
+  private static void printWelcomeMessage() {
+    System.out.println(UIMessages.DOTS);
+    System.out.println(UIMessages.WELCOME_MESSAGE);
+    System.out.println(UIMessages.DOTS);
+    System.out.println(UIMessages.INPUT_MESSAGE);
   }
 }
---
+++ UIMessages/src/UIMessages.java
@@ -0,0 +1,16 @@
+public class UIMessages {
+
+  public static final String DOTS = "------------------------------------------------";
+  public static final String WELCOME_MESSAGE = "Welcome to the Eternity Functions \n"
+      + "This Program will calculate value of Sinh(x) for given x";
+  public static final String INPUT_MESSAGE = "Please enter any number as input\n";
+  public static final String ERROR_MESSAGE = "Given input is in valid, Input should be number only";
+  public static final String SUCCESS_MESSAGE = "Output for the given input ";
+  public static final String OPTIONS_MESSAGE = "Please enter 1 to continue\nPlease enter 0 to exit from program";
+  public static final String ERROR_MESSAGE_RANGE =
+      "Value should be more than " + Double.MIN_VALUE + " and less than " + Double.MAX_VALUE;
+  public static final String ERROR_MESSAGE_RANGE_OUTPUT_MAX =
+      "Output value is more than supported value " + Double.MAX_VALUE + "";
+  public static final String ERROR_MESSAGE_RANGE_OUTPUT_MIN =
+      "Output value is less than supported value " + Double.MIN_VALUE + "";
+}
