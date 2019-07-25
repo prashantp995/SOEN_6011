e90aaa7acf723ba27eb5797e73c25423f2f7c1b7
DESKTOP-S31NHKM\prash <prashantp95@gmail.com>
2019-07-25 13:23:05 -0400

    restricted access for the class

--- UIMessages/src/UIMessages.java
+++ UIMessages/src/UIMessages.java
@@ -1,18 +1,18 @@
-public class UIMessages {
+class UIMessages {
 
-  public static final String DOTS = "------------------------------------------------";
-  public static final String WELCOME_MESSAGE = "Welcome to the Eternity Functions \n"
+  static final String DOTS = "------------------------------------------------";
+  static final String WELCOME_MESSAGE = "Welcome to the Eternity Functions \n"
       + "This Program will calculate value of Sinh(x) for given x";
-  public static final String INPUT_MESSAGE = "Please enter any number as input\n";
-  public static final String ERROR_MESSAGE = "Given input is in valid, Input should be number only";
-  public static final String SUCCESS_MESSAGE = "Output for the given input ";
-  public static final String OPTIONS_MESSAGE = "Please enter 1 to continue\nPlease enter 0 to exit from calculator program";
-  public static final String ERROR_MESSAGE_RANGE =
+  static final String INPUT_MESSAGE = "Please enter any number as input\n";
+  static final String ERROR_MESSAGE = "Given input is in valid, Input should be number only";
+  static final String SUCCESS_MESSAGE = "Output for the given input ";
+  static final String OPTIONS_MESSAGE = "Please enter 1 to continue\nPlease enter 0 to exit from calculator program";
+  static final String ERROR_MESSAGE_RANGE =
       "Value should be more than " + Double.MIN_VALUE + " and less than " + Double.MAX_VALUE;
-  public static final String ERROR_MESSAGE_RANGE_OUTPUT_MAX =
+  static final String ERROR_MESSAGE_RANGE_OUTPUT_MAX =
       "Output value is more than supported value " + Double.MAX_VALUE + "";
-  public static final String ERROR_MESSAGE_RANGE_OUTPUT_MIN =
+  static final String ERROR_MESSAGE_RANGE_OUTPUT_MIN =
       "Output value is less than supported value " + Double.MIN_VALUE + "";
-  public static final String ERROR_INVALID_CHOICE =
+  static final String ERROR_INVALID_CHOICE =
       "invalid choice, please follow the instruction";
 }
